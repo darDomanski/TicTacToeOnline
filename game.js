baabab9ecb736af3f23f126f75d358d93fcf6911
Roman Bulinski <aboandrus@gmail.com>
2018-11-06 09:25:59 +0100

    Change names of button

--- game.js
+++ game.js
@@ -1,110 +1,126 @@
 
 
+
 function check(){
+    //create variabels
+    // for (var i=1;i<4;i++){
+    //     var a = "a"+i;
+    //     var a = document.getElementById(a).className;
+    //     var b = "b"+i;
+    //     var b = document.getElementById(b).className;
+    //     var c = "a"+i;
+    //     var c = document.getElementById(c).className;
+    // }
 
-    if( document.getElementById("a1").className === "cell_yellow" &&
-        document.getElementById("a2").className === "cell_yellow" &&
-        document.getElementById("a3").className === "cell_yellow"    ){
-        alert("yellow won !!!")
+    // console.log(a1);
+    // console.log(a2);
+    // console.log(a3);
+
+    if( document.getElementById("a1").className === "crosses" &&
+        document.getElementById("a2").className === "crosses" &&
+        document.getElementById("a3").className === "crosses"    ){
+        alert("crosses won !!!")
+      
     }
 
-    if( document.getElementById("b1").className === "cell_yellow" &&
-        document.getElementById("b2").className === "cell_yellow" &&
-        document.getElementById("b3").className === "cell_yellow"    ){
-        alert("yellow won !!!")
+    if( document.getElementById("b1").className === "crosses" &&
+        document.getElementById("b2").className === "crosses" &&
+        document.getElementById("b3").className === "crosses"    ){
+        alert("crosses won !!!")
     }
 
-    if( document.getElementById("c1").className === "cell_yellow" &&
-        document.getElementById("c2").className === "cell_yellow" &&
-        document.getElementById("c3").className === "cell_yellow"    ){
-        alert("yellow won !!!")
+    if( document.getElementById("c1").className === "crosses" &&
+        document.getElementById("c2").className === "crosses" &&
+        document.getElementById("c3").className === "crosses"    ){
+        alert("crosses won !!!")
     }
 
-    if( document.getElementById("a1").className === "cell_yellow" &&
-        document.getElementById("b1").className === "cell_yellow" &&
-        document.getElementById("c1").className === "cell_yellow"    ){
-        alert("yellow won !!!")
+    if( document.getElementById("a1").className === "crosses" &&
+        document.getElementById("b1").className === "crosses" &&
+        document.getElementById("c1").className === "crosses"    ){
+        alert("crosses won !!!")
     }
 
-    if( document.getElementById("a2").className === "cell_yellow" &&
-        document.getElementById("b2").className === "cell_yellow" &&
-        document.getElementById("c2").className === "cell_yellow"    ){
-        alert("yellow won !!!")
+    if( document.getElementById("a2").className === "crosses" &&
+        document.getElementById("b2").className === "crosses" &&
+        document.getElementById("c2").className === "crosses"    ){
+        alert("crosses won !!!")
     }
 
-    if( document.getElementById("a3").className === "cell_yellow" &&
-        document.getElementById("b3").className === "cell_yellow" &&
-        document.getElementById("c3").className === "cell_yellow"    ){
-        alert("yellow won !!!")
+    if( document.getElementById("a3").className === "crosses" &&
+        document.getElementById("b3").className === "crosses" &&
+        document.getElementById("c3").className === "crosses"    ){
+        alert("crosses won !!!")
     }
 
-    if( document.getElementById("a1").className === "cell_yellow" &&
-        document.getElementById("b2").className === "cell_yellow" &&
-        document.getElementById("c3").className === "cell_yellow"    ){
-        alert("yellow won !!!")
+    if( document.getElementById("a1").className === "crosses" &&
+        document.getElementById("b2").className === "crosses" &&
+        document.getElementById("c3").className === "crosses"    ){
+        alert("crosses won !!!")
     }
 
-    if( document.getElementById("a3").className === "cell_white" &&
-        document.getElementById("b2").className === "cell_white" &&
-        document.getElementById("c1").className === "cell_white"    ){
-        alert("yellow won !!!")
-    }
-
-
-
-
-    if( document.getElementById("a1").className === "cell_white" &&
-        document.getElementById("a2").className === "cell_white" &&
-        document.getElementById("a3").className === "cell_white"    ){
-        alert("white won !!!")
-    }
-
-    if( document.getElementById("b1").className === "cell_white" &&
-        document.getElementById("b2").className === "cell_white" &&
-        document.getElementById("b3").className === "cell_white"    ){
-        alert("white won !!!")
-    }
-
-    if( document.getElementById("c1").className === "cell_white" &&
-        document.getElementById("c2").className === "cell_white" &&
-        document.getElementById("c3").className === "cell_white"    ){
-        alert("white won !!!")
-    }
-
-    if( document.getElementById("a1").className === "cell_white" &&
-        document.getElementById("b1").className === "cell_white" &&
-        document.getElementById("c1").className === "cell_white"    ){
-        alert("white won !!!")
-    }
-
-    if( document.getElementById("a2").className === "cell_white" &&
-        document.getElementById("b2").className === "cell_white" &&
-        document.getElementById("c2").className === "cell_white"    ){
-        alert("white won !!!")
-    }
-
-    if( document.getElementById("a3").className === "cell_white" &&
-        document.getElementById("b3").className === "cell_white" &&
-        document.getElementById("c3").className === "cell_white"    ){
-        alert("white won !!!")
-    }
-
-    if( document.getElementById("a1").className === "cell_white" &&
-        document.getElementById("b2").className === "cell_white" &&
-        document.getElementById("c3").className === "cell_white"    ){
-        alert("white won !!!")
-    }
-
-    if( document.getElementById("a3").className === "cell_white" &&
-        document.getElementById("b2").className === "cell_white" &&
-        document.getElementById("c1").className === "cell_white"    ){
-        alert("white won !!!")
+    if( document.getElementById("a3").className === "crosses" &&
+        document.getElementById("b2").className === "crosses" &&
+        document.getElementById("c1").className === "crosses"    ){
+        alert("crosses won !!!")
     }
 
 
+    if( document.getElementById("a1").className === "noughts" &&
+        document.getElementById("a2").className === "noughts" &&
+        document.getElementById("a3").className === "noughts"    ){
+        alert("noughts won !!! !!!")
+    }
+
+    if( document.getElementById("b1").className === "noughts" &&
+        document.getElementById("b2").className === "noughts" &&
+        document.getElementById("b3").className === "noughts"    ){
+        alert("noughts won !!! !!!")
+    }
+
+    if( document.getElementById("c1").className === "noughts" &&
+        document.getElementById("c2").className === "noughts" &&
+        document.getElementById("c3").className === "noughts"    ){
+        alert("noughts won !!! !!!")
+    }
+
+    if( document.getElementById("a1").className === "noughts" &&
+        document.getElementById("b1").className === "noughts" &&
+        document.getElementById("c1").className === "noughts"    ){
+        alert("noughts won !!! !!!")
+    }
+
+    if( document.getElementById("a2").className === "noughts" &&
+        document.getElementById("b2").className === "noughts" &&
+        document.getElementById("c2").className === "noughts"    ){
+        alert("noughts won !!! !!!")
+    }
+
+    if( document.getElementById("a3").className === "noughts" &&
+        document.getElementById("b3").className === "noughts" &&
+        document.getElementById("c3").className === "noughts"    ){
+        alert("noughts won !!! !!!")
+    }
+
+    if( document.getElementById("a1").className === "noughts" &&
+        document.getElementById("b2").className === "noughts" &&
+        document.getElementById("c3").className === "noughts"    ){
+        alert("noughts won !!! !!!")
+    }
+
+    if( document.getElementById("a3").className === "noughts" &&
+        document.getElementById("b2").className === "noughts" &&
+        document.getElementById("c1").className === "noughts"    ){
+        alert("noughts won !!! !!!")
+    }
 }
 
 
+var cells = { a1:"empty", a2:"empty", a3:"empty",
+                b1:"empty", b2:"empty", b3:"empty",
+                c1:"empty", c2:"empty", c3:"empty", }
+
+console.log(cells);
 
 var counter = 0
 var flag =0;
@@ -112,37 +128,32 @@ var flag =0;
 function move(){
     var id_cell = event.srcElement.id;
     // console.log(id_cell);
-    if( document.getElementById(id_cell).className === "cell_black" ){
+    if( document.getElementById(id_cell).className === "empty" ){
 
         if ( flag === 0){
             counter++;
-            document.getElementById(id_cell).className = "cell_yellow";
+            document.getElementById(id_cell).className = "crosses";
+            console.log("cell id : "+cells.id_cell);
+            cells.id_cell = "crosses";
             console.log( document.getElementById(id_cell).className  );
+            console.log(cells);
             flag =1;
         } else if (flag === 1) {
             counter++;
-            document.getElementById(id_cell).className = "cell_white";
+            document.getElementById(id_cell).className = "noughts";
+            cells.id_cell = "noughts"
             console.log( document.getElementById(id_cell).className  );
+            console.log(cells);
             flag = 0;
         }
         check();
     } else  { alert("is prohibited"); }
 }
 
-var cell = document.getElementsByClassName("cell_black");
+var cell = document.getElementsByClassName("empty");
 for(j=0;j<cell.length;j++){
     cell[j].addEventListener('click', move, false);
 }
-
-// document.getElementById("a1").addEventListener('click', move);
-// document.getElementById("a2").addEventListener('click', move);
-// document.getElementById("a3").addEventListener('click', move);
-// document.getElementById("b1").addEventListener('click', move);
-// document.getElementById("b2").addEventListener('click', move);
-// document.getElementById("b3").addEventListener('click', move);
-// document.getElementById("c1").addEventListener('click', move);
-// document.getElementById("c2").addEventListener('click', move);
-// document.getElementById("c3").addEventListener('click', move);
 
 
    
