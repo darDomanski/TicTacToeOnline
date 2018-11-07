647315d7cea1b1ad77b499cc0907900fa09ddda3
dardomanski <dardomanski@gmail.com>
2018-11-07 10:24:39 +0100

    added initDataBase() function

--- login.js
+++ login.js
@@ -7,3 +7,30 @@ var config = {
     messagingSenderId: "903488103673"
 };
 firebase.initializeApp(config);
+
+function initDataBase() {
+    firebase.database().ref().once('value', function (snapshot) {
+
+        if (!snapshot.exists()) {
+            var initialConfig = {
+                p1: "",
+                p2: "",
+                isReady: false,
+                turn: "p1"
+            };
+            database.push(initialConfig)
+            console.log("Data base not found! Initialized new data base.")
+        }
+
+    });
+};
+
+function setNewValueToDataBaseRecord(recordName, newValue) {
+    let key = this.getKeyOfDataBase();
+
+
+
+    var recordRef = database.ref("game_data/" + key + "/" + recordName);
+    recordRef.set(newValue);
+    console.log(recordName + " changed to new value: " + newValue);
+};
