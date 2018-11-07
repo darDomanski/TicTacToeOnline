0be81c8ea9f755b8b2934c4631e0b020f8b565cb
dardomanski <dardomanski@gmail.com>
2018-11-07 10:20:38 +0100

    added firebase script

---
+++ login.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyD187jIjmdW-ajoiJHAo_NT7yDrZjhuj_s",
+    authDomain: "tictactoe-b6020.firebaseapp.com",
+    databaseURL: "https://tictactoe-b6020.firebaseio.com",
+    projectId: "tictactoe-b6020",
+    storageBucket: "",
+    messagingSenderId: "903488103673"
+};
+firebase.initializeApp(config);
