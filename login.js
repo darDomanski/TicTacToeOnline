c563e687afdc2ffaba9c7e2473f92bd1e094bec6
dardomanski <dardomanski@gmail.com>
2018-11-08 09:52:11 +0100

    added method to find  a game to join

--- login.js
+++ login.js
@@ -8,56 +8,100 @@ var config = {
 };
 firebase.initializeApp(config);
 
+
+
 var loginBtn = document.getElementById("log_in_btn");
 loginBtn.addEventListener("click", initDataBase, false);
+// var recordId;
+var gameId;
+var gameConfig = {
+    player1: { name: "" },
+    player2: { name: "" },
+    fields: {
+        a1: "empty", a2: "empty", a3: "empty",
+        b1: "empty", b2: "empty", b3: "empty",
+        c1: "empty", c2: "empty", c3: "empty"
+    },
+    turn: "",
+    isReady: false
+};
+var playerName;
+var gameDataBaseRef = firebase.database().ref("games");
 
 function initDataBase() {
-    console.log("Works");
+
+    // Check if game's database exists, create one if not
     firebase.database().ref().once('value', function (snapshot) {
+        if (!snapshot.exists() || (snapshot.exists() && firebase.database().ref("games/") == true)) {
+            playerName = document.getElementById("player_name").value;
+            gameConfig.turn = playerName;
 
-        if (!snapshot.exists()) {
-            var initialConfig = {
-                player1: { name: "", score: 0 },
-                player2: { name: "", score: 0 },
-                fields: {
-                    a1: "empty", a2: "empty", a3: "empty",
-                    b1: "empty", b2: "empty", b3: "empty",
-                    c1: "empty", c2: "empty", c3: "empty"
-                },
-                turn: "player1",
-                isReady: false
-            };
-            var id = firebase.database().ref().push(initialConfig)
-            console.log(id + "Data base not found! Initialized new game data base.")
-
-            var gamedata = id.ref();
-            gamedata.push({ foo: "foo" });
+            gameConfig.player1 = { name: playerName };
+            id = firebase.database().ref("games").push(gameConfig);
+            gameId = id.key;
+            console.log("Data base not found! Initialized new game data base. You are player1, waiting for player2...")
         }
-
     });
+
+    // Get to last game's database and check if is not ready isReady
+
 };
 
 function setNewValueToDataBaseRecord(recordName, newValue) {
-    let key = this.getKeyOfDataBase();
-
-    var recordRef = database.ref("game_data/" + key + "/" + recordName);
-    recordRef.set(newValue);
+    // let key = getKeyOfDataBase();
+    gameConfig[recordName] = newValue;
+    let recordRef = firebase.database().ref("games/" + gameId);
+    recordRef.push(gameConfig);
     console.log(recordName + " changed to new value: " + newValue);
 };
 
+// function setPlayernameData(playerNumber, newName) {
+//      player = {
+//         name: newName
+//     };
+
+//     var playerNameRef = firebase.database().ref("games/" + gameId + "/player" + playerNumber);
+//     playerNameRef.set(player);
+// };
 function getKeyOfDataBase() {
-    let records,
-        keys,
-        key,
-        database;
-    database = firebase.ref("game_data");
+    // var records,
+    //     keys,
+    var keyToReturn = "";
+    var database = firebase.database().ref("games");
+    // console.log(database);
+    var key;
 
     database.on('value', function (data) {
-        records = data.val();
-        keys = Object.keys(records);
-        key = keys[0];
+        var records = data.val();
+        var keys = Object.keys(records);
+        key = keys[keys.length - 1];
     });
 
     return key;
-}
+
+};
+
+function seekForGameToJoin() {
+    var database = getDatabaseFromFirebase();
+    var games = database.games;
+
+    for (var key in games) {
+        if (games[key].isReady == false) {
+            gameId = key;
+            return true;
+        }
+    }
+
+};
+
+function getDatabaseFromFirebase() {
+    var gamesDatabase = {};
+    gameDataBaseRef.on('value', function (snapshot) {
+        gamesDatabase[snapshot.key] = snapshot.val();
+    });
+    return gamesDatabase;
+};
+
+seekForGameToJoin();
+
 
